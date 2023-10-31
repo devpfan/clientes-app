334049bdc1dd9f2d5696d1687bc2fb8e210540ea
devpfan <128647947+devpfan@users.noreply.github.com>
2023-10-31 15:02:16 -0500

    assets

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json';
 import {Cliente} from './cliente';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class ClienteService {
 
-  constructor() { }
+  constructor(){}
 
-  getClientes(): Cliente[]{
-    return CLIENTES;
+  getClientes(): Observable<Cliente[]>{
+    return of(CLIENTES) ;
   }
-}
+
+ }
