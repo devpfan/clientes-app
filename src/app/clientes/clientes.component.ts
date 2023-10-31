96becdc168a5510cf15ac7fe0ad63adbe380b95f
devpfan <128647947+devpfan@users.noreply.github.com>
2023-10-31 13:32:11 -0500

    clase cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+import {CLIENTES} from './clientes.json';
+ 
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
- 
+  templateUrl: './clientes.component.html'
 })
-export class ClientesComponent {
 
+export class ClientesComponent implements OnInit{
+  
+  clientes: Cliente[];
+
+
+  constructor(){}
+  
+  ngOnInit() {
+    this.clientes=CLIENTES;
+  }
+
+  
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import {Cliente} from './cliente';
+
+
+export const CLIENTES: Cliente[]= [
+    {id:1, nombre:'Paulo', apellido:'Martinez', email:'pfabian@outlook.com', createAt:'2023-10-31'},
+    {id:2, nombre:'Sofi', apellido:'Martinez', email:'sofi@outlook.com', createAt:'2023-10-27'},
+    {id:3, nombre:'Laika', apellido:'Martinez', email:'azabache@outlook.com', createAt:'2023-10-31'},
+    {id:4, nombre:'Ela', apellido:'Sanchez', email:'eims@outlook.com', createAt:'2023-10-30'}
+    ];
